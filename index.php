ee272de0b419eca4182f78ee549e0b86a0337c8d
ahmelevs <andrejs.hmelovs@gmail.com>
2023-10-07 08:52:01 +0400

    Fix preload links

--- index.php
+++ index.php
@@ -9,19 +9,12 @@
     <meta name="keywords" content="furniture, select furniture, Home Centre, IKEA, Home Box, Danube Home, OC Home, West Elm, Pottery Barn">
     <link rel="canonical" href="https://widget.biglayoutdata.com" />
 
+    <link rel="preload" as="script" href="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript">
+    <link rel="preload" as="script" href="widget.js" type="text/javascript">
+    <link rel="preload" as="style" href="widget.css" type="text/css">
+    <link rel="preload" as="font" href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" type="font/woff2">
     <link rel="preload" as="image" href="https://space.biglayoutdata.com/pics/contemporary.webp" type="image/webp">
     <link rel="preload" as="image" href="https://space.biglayoutdata.com/pics/neoclassic.webp" type="image/webp">
-    
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHFE18GNY6"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-VHFE18GNY6');
-    </script>
-    <!-- Google tag (gtag.js) -->
 </head>
 <body>
     <div id="logo" style="max-width: 165px; margin-left: calc(50% - 173px);">
@@ -35,7 +28,7 @@
     <script type="text/javascript">
         jQuery(document).ready(function($) {
             var scriptElement = document.createElement('script');
-            scriptElement.src = 'widget.js?_' + Date.now();
+            scriptElement.src = 'widget.js';
             scriptElement.type = 'text/javascript';
             scriptElement.async = true;
             scriptElement.onload = function() {
@@ -43,14 +36,21 @@
                 wbld.init(id='wbld', widget_name='wbld_widget', url_params=url_params);
             };
             document.body.appendChild(scriptElement);
-
-            var styleElement = document.createElement('link');
-            styleElement.href = 'widget.css?_' + Date.now();
-            styleElement.rel = 'stylesheet';
-            styleElement.type = 'text/css';
-            document.head.appendChild(styleElement);
         });
     </script>
+    <link rel="stylesheet" href="widget.css" type="text/css" />
     <!-- BigLayoutData widget (widget.js) -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHFE18GNY6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VHFE18GNY6');
+        gtag('config', 'AW-11334053187');
+    </script>
+    <!-- Google tag (gtag.js) -->
 </body>
 </html>
